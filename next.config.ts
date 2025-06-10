3bbaacdeb7e143aa2821e1e9518cc40e2a1ea677
hua.sun03 <hua.sun03@ninebot.com>
2025-06-10 13:44:12 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverActions: {
-      bodySizeLimit: "2mb",
-    },
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-        dns: false,
-        child_process: false,
-        perf_hooks: false,
-      };
-    }
-    return config;
-  },
+  /* config options here */
+  output: "export",
 };
 
 export default nextConfig;
